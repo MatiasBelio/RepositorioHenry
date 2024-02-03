12a9c472956641a3288da05cba2f18dbc44200f1
MatiasBelio <matibelio@hotmail.com>
2024-02-03 00:50:17 -0300

    HOMEWORK MODULO 6

---
+++ MODULO 6/M06 Bucles/ejercicios/21.js
@@ -0,0 +1,24 @@
+function esPotenciaDeDos(numero) {
+  // La función recibe un "numero" por argumento.
+  // Determina si es una potencia de 2.
+  // Devuelve true si lo es, sino devuelve false.
+  // PISTA: Utiliza un bucle while.
+  // Tu código:
+  
+//   while (numero > 1); {
+//     if (numero % 2 !== 0) {
+//       return false;
+// }    else {
+//       numero = numero / 2; 
+//   }
+// }
+
+// return true;   
+while (numero % 2 === 0) {
+  numero /= 2;
+}
+return numero === 1;
+
+
+}
+module.exports = esPotenciaDeDos;
---
+++ MODULO 6/M06 Bucles/ejercicios/22.js
@@ -0,0 +1,16 @@
+function diasEnMes(mes) {
+  // La función recibe un mes(número) por argumento.
+  // Determine cúantos días tiene el mes correspondiente a ese número.
+  // PISTA: Usa un bucle do-while para validar que el mes ingresado sea válido.
+  // Tu código:
+var diasMes = new Date(2023, mes, 0).getDate();
+  do {
+    if ( mes > 0 && mes < 13) return diasMes;
+} while (mes > 0 && mes < 13);
+return 0;
+
+
+
+}
+
+module.exports = diasEnMes;
